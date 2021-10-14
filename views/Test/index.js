8e6f047c58ea07048f3317b22cb34bb70b4d9fc2
maxgraaab <51647099+maxgraaab@users.noreply.github.com>
2021-10-14 23:29:41 +0800

    dummy table able to create edit and delete

--- views/Test/index.js
+++ views/Test/index.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useSnackbar } from "notistack";
+import { unflatten } from "flat";
 
 //*lodash
 import has from "lodash/has";
+import omit from "lodash/omit";
+import find from "lodash/find";
+import trim from "lodash/trim";
+import isEmpty from "lodash/isEmpty";
+import forOwn from "lodash/forOwn";
 
 //*components
 import { CustomIcon } from "components/Icons";
 
 //*material-ui
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import { makeStyles } from "@mui/styles";
+import IconButton from "@mui/material/IconButton";
 
 //*assets
 
@@ -24,37 +31,59 @@ import axios from "utils/http-anxios";
 import useSwrHttp from "utils/useSwrHttp";
 
 //*styles
-const useStyles = makeStyles({
-  root: {
-    "& .cold": {
-      backgroundColor: "#b9d5ff91",
-      color: "#1a3e72",
-    },
-    "& .hot": {
-      backgroundColor: "#ff943975",
-      color: "#1a3e72",
-    },
-  },
-});
 
 //*custom components
 
 function Test() {
   //*define
-  const classes = useStyles();
   const { data, mutate, isValidating } = useSwrHttp("dummy");
+  const [selectionModel, setSelectionModel] = useState([]);
   const { enqueueSnackbar } = useSnackbar();
 
   //*states
   const [editedData, setEditedData] = useState({});
 
   //*const
+
+  const RenderCell = ({ field, formattedValue, api, id }) => {
+    const isEdited = has(editedData, `${id}.${field}`);
+    const findData = find(data, { id: id }) || "";
+    const handleClickUndoButton = () => {
+      handleUndoEditData(id, field);
+      api.setEditCellValue({ id, field, value: findData[`${field}`] });
+      api.commitCellChange({ id, field });
+      api.setCellMode(id, field, "view");
+    };
+
+    return (
+      <Box
+        bgcolor={isEdited ? "#ff943975" : "inherit"}
+        width={isEdited ? "100%" : "auto"}
+      >
+        <Box display="flex" justifyContent="space-between">
+          <Box textOverflow="ellipsis" overflow="hidden" pl={2} pr={2}>
+            {formattedValue}
+          </Box>
+          {isEdited && (
+            <Box>
+              <IconButton size="small" onClick={handleClickUndoButton}>
+                <CustomIcon size="small" icon="undo" />
+              </IconButton>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    );
+  };
+
   const columns = [
     {
       field: "dataBoolean",
       headerName: "Boolean",
       type: "boolean",
       editable: true,
+      width: 115,
+      renderCell: RenderCell,
     },
     {
       field: "dataDate",
@@ -62,14 +91,15 @@ function Test() {
       type: "dateTime",
       width: 240,
       editable: true,
+      renderCell: RenderCell,
     },
     {
       field: "dataString",
       headerName: "String",
       type: "string",
-      flex: 1,
-      minWidth: 300,
+      minWidth: 400,
       editable: true,
+      renderCell: RenderCell,
     },
   ];
 
@@ -88,8 +118,11 @@ function Test() {
     };
 
     try {
-      const post = await axios.post("dummy", newData);
-      mutate([post.data, ...data], false);
+      const data = await axios.post("dummy", newData);
+      mutate();
+      enqueueSnackbar(data.statusText, {
+        variant: "success",
+      });
     } catch ({ response }) {
       const errorMessage = response.data.message;
       enqueueSnackbar(errorMessage, {
@@ -99,25 +132,92 @@ function Test() {
   };
 
   const handleEditNewDummy = (editData) => {
-    const { row, field, value, id } = editData;
-    const rowData = row[field];
-    if (rowData !== editData[`${id}.${field}`])
+    const { field, value, id } = editData;
+    const findData = find(data, { id: id }) || "";
+    if (trim(value) !== trim(findData[`${field}`]))
       setEditedData({ ...editedData, [`${id}.${field}`]: value });
+    else {
+      setEditedData({ ...omit(editedData, [`${id}.${field}`]) });
+    }
   };
+
+  const handleUndoEditData = (id, field) => {
+    setEditedData({ ...omit(editedData, [`${id}.${field}`]) });
+  };
+
+  const handleSaveAll = async () => {
+    const unflatEditData = unflatten(editedData);
+    const allPromises = [];
+    forOwn(unflatEditData, (data, key) => {
+      allPromises.push(axios.patch(`dummy/${key}`, data));
+    });
+    const editRes = await Promise.all(allPromises);
+    editRes.forEach((data) => {
+      enqueueSnackbar(data.statusText, {
+        variant: "success",
+      });
+    });
+    setEditedData({});
+  };
+
+  const handleDelete = async () => {
+    const allPromises = [];
+
+    selectionModel.forEach((key) =>
+      allPromises.push(axios.delete(`dummy/${key}`))
+    );
+
+    const deleteRes = await Promise.all(allPromises);
+    deleteRes.forEach((data) => {
+      enqueueSnackbar(data.statusText, {
+        variant: "success",
+      });
+    });
+    setEditedData({});
+    mutate();
+  };
+
+  const handleCellKeyDown = useCallback((params, event) => {
+    if (!["Escape", "Delete", "Backspace", "Enter"].includes(event.key)) {
+      event.defaultMuiPrevented = true;
+    }
+  }, []);
 
   return (
     <Box style={{ minHeight: 400, width: "100%" }}>
       <Box style={{ display: "flex", height: "100%" }}>
-        <Box style={{ flexGrow: 1 }} className={classes.root}>
+        <Box style={{ flexGrow: 1 }}>
           <Box pb={2}>
-            <Button
-              onClick={handleAddNewDummy}
-              size="small"
-              variant="contained"
-              startIcon={<CustomIcon icon="add" color="white" />}
-            >
-              New
-            </Button>
+            <Stack spacing={2} direction="row">
+              <Button
+                onClick={handleAddNewDummy}
+                size="small"
+                variant="contained"
+                startIcon={<CustomIcon icon="add" color="white" />}
+              >
+                New
+              </Button>
+              {!isEmpty(editedData) && (
+                <Button
+                  size="small"
+                  onClick={handleSaveAll}
+                  variant="contained"
+                  startIcon={<CustomIcon icon="save" color="white" />}
+                >
+                  Save
+                </Button>
+              )}
+              {!isEmpty(selectionModel) && (
+                <Button
+                  size="small"
+                  onClick={handleDelete}
+                  variant="contained"
+                  startIcon={<CustomIcon icon="save" color="white" />}
+                >
+                  Delete {`(${selectionModel.length})`}
+                </Button>
+              )}
+            </Stack>
           </Box>
           {data && data.length > 0 && (
             <DataGrid
@@ -128,13 +228,14 @@ function Test() {
               columns={columns}
               checkboxSelection
               disableSelectionOnClick
+              editMode="cell"
               density="compact"
               onCellEditCommit={handleEditNewDummy}
-              getCellClassName={(params) => {
-                if (has(editedData, `${params.id}.${params.field}`)) {
-                  return "hot";
-                }
+              onCellKeyDown={handleCellKeyDown}
+              onSelectionModelChange={(newSelectionModel) => {
+                setSelectionModel(newSelectionModel);
               }}
+              selectionModel={selectionModel}
             />
           )}
         </Box>
